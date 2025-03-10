44227b24fdbf72bf9f9b34e0705f1ff6368341f9
MattSona99 <matteo.sonaglioni@gmail.com>
2025-03-10 10:55:49 +0100

    Aggiunta file tasks

--- BDAchallenge2425/main.py
+++ BDAchallenge2425/main.py
@@ -1,2 +1,11 @@
 import os
 
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+
+entry_point = 'hdfs://user/amircoli/BDAchallenge2324'
+save_point = 'hdfs://user/amircoli/Scrivania/BDA/spark2425/results/gruppo_1'
+
